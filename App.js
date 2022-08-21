fab905e833463d782b876044fe6c8cac0e643a91
Odwambombo <99673174+Odwambombo@users.noreply.github.com>
2022-08-21 10:24:22 +0200

    app.js
    latest modification to remove eval()

--- App.js
+++ App.js
@@ -2,17 +2,28 @@
 function clearScreen() {
     document.getElementById("result").value = "";
 }
- 
+
+const button = document.getElementById('dot');
+
 // This function display values
 function display(value) {
+
+// Check if the comma is already press so you can not press it again
+    if(value === '.' ){
+        button.disabled = true;
+    }else{
+        button.disabled = false; 
+    }
+
     document.getElementById("result").value += value;
 }
  
 // This function evaluates the expression and returns result
 function calculate() {
-    var p = document.getElementById("result").value;
-    var q = eval(p);
-    document.getElementById("result").value = q;
+    let inputValue = document.getElementById("result").value;
+    let result = Function("return " + inputValue)();
+    
+    document.getElementById("result").value = result;
 
 }
 
